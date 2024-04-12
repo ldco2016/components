ea62c4ce4f361c1688c1064a16fe7e9ff6332aa3
Daniel Cortes <danielcortes@Daniels-MacBook-Pro.local>
2024-04-12 07:15:13 -0500

    Completed props and added new component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail author='Sam' />
-      <CommentDetail author='Alex' />
-      <CommentDetail author='Jane' />
+      <ApprovalCard />
+      <CommentDetail
+        author='Sam'
+        timeAgo='Today at 4:45PM'
+        content='Nice blog post'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='Alex'
+        timeAgo='Today at 2:00AM'
+        content='I like this subject'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='Jane'
+        timeAgo='Today at 3:00PM'
+        content='That is wild'
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
